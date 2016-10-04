a3e25092a1b6a8a60f3341b722bd0cd5f6d4e68d
Andrew Jack <andrew.jack@drivetribe.com>
2016-10-04 10:31:20 +0100

    Simplify android project

--- Android/src/main/java/com/tkporter/fabrictwitterkit/FabricTwitterKitModule.java
+++ Android/src/main/java/com/tkporter/fabrictwitterkit/FabricTwitterKitModule.java
@@ -4,38 +4,31 @@ import android.app.Activity;
 import android.content.Intent;
 
 import com.facebook.react.bridge.ActivityEventListener;
+import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.ReadableMap;
-import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.bridge.WritableNativeMap;
-import com.google.gson.Gson;
-
-import com.twitter.sdk.android.tweetcomposer.TweetComposer;
 import com.twitter.sdk.android.core.Result;
 import com.twitter.sdk.android.core.TwitterCore;
 import com.twitter.sdk.android.core.TwitterException;
 import com.twitter.sdk.android.core.TwitterSession;
 import com.twitter.sdk.android.core.identity.TwitterLoginButton;
-import com.twitter.sdk.android.core.TwitterApiClient;
-import com.twitter.sdk.android.core.models.User;
-import retrofit2.http.GET;
-import retrofit2.http.Query;
+import com.twitter.sdk.android.tweetcomposer.TweetComposer;
 
 
-public class FabricTwitterKitModule extends ReactContextBaseJavaModule implements ActivityEventListener {
+class FabricTwitterKitModule extends ReactContextBaseJavaModule implements ActivityEventListener {
 
-    public TwitterLoginButton loginButton;
-    private final ReactApplicationContext reactContext;
-    private Callback callback = null;
-    //112 is the average ascii value for every letter in 'twitter'
+    // 112 is the average ascii value for every letter in 'twitter'
     private static final int REQUEST_CODE = 112112;
 
-    public FabricTwitterKitModule(ReactApplicationContext reactContext) {
+    private TwitterLoginButton loginButton;
+    private Callback callback = null;
+
+    FabricTwitterKitModule(ReactApplicationContext reactContext) {
         super(reactContext);
-        this.reactContext = reactContext;
     }
 
 
@@ -65,7 +58,7 @@ public class FabricTwitterKitModule extends ReactContextBaseJavaModule implement
     public void onNewIntent(Intent intent) {
     }
 
-    public void sendCallback(Boolean completed, Boolean cancelled, Boolean error) {
+    private void sendCallback(Boolean completed, Boolean cancelled, Boolean error) {
         if (callback != null) {
             callback.invoke(completed, cancelled, error);
             callback = null;
@@ -79,12 +72,14 @@ public class FabricTwitterKitModule extends ReactContextBaseJavaModule implement
 
             String body = options.hasKey("body") ? options.getString("body") : "";
 
-            TweetComposer.Builder builder = new TweetComposer.Builder(reactContext).text(body);
-            final Intent intent = builder.createIntent();
-            reactContext.startActivityForResult(intent, REQUEST_CODE, intent.getExtras());
+            ReactApplicationContext reactContext = getReactApplicationContext();
+            if (reactContext != null) {
+                TweetComposer.Builder builder = new TweetComposer.Builder(reactContext).text(body);
+                final Intent intent = builder.createIntent();
+                reactContext.startActivityForResult(intent, REQUEST_CODE, intent.getExtras());
+            }
 
         } catch (Exception e) {
-            //error!
             sendCallback(false, false, true);
             throw e;
         }
@@ -99,8 +94,8 @@ public class FabricTwitterKitModule extends ReactContextBaseJavaModule implement
             public void success(Result<TwitterSession> sessionResult) {
                 WritableMap result = new WritableNativeMap();
                 result.putString("authToken", sessionResult.data.getAuthToken().token);
-                result.putString("authTokenSecret",sessionResult.data.getAuthToken().secret);
-                result.putString("userID", sessionResult.data.getUserId()+"");
+                result.putString("authTokenSecret", sessionResult.data.getAuthToken().secret);
+                result.putString("userID", sessionResult.data.getUserId() + "");
                 result.putString("userName", sessionResult.data.getUserName());
                 callback.invoke(null, result);
             }
@@ -116,58 +111,12 @@ public class FabricTwitterKitModule extends ReactContextBaseJavaModule implement
     }
 
     @ReactMethod
-    public void fetchProfile(final Callback callback) {
-
-        try {
-            ReactNativeFabricApiClient client = new ReactNativeFabricApiClient(TwitterCore.getInstance().getSessionManager().getActiveSession());
-            client.getCustomService().show(TwitterCore.getInstance().getSessionManager().getActiveSession().getUserId(), new com.twitter.sdk.android.core.Callback<User>() {
-                @Override
-                public void success(Result<User> result) {
-                    Gson gson = new Gson();
-                    WritableMap map = gson.fromJson(gson.toJson(result), WritableMap.class);
-                    callback.invoke(null, map);
-                }
-
-                @Override
-                public void failure(TwitterException exception) {
-                    exception.printStackTrace();
-                    TwitterCore.getInstance().getSessionManager().clearActiveSession();
-                    callback.invoke(exception.getMessage());
-                }
-            });
-        } catch (Exception ex) {
-            callback.invoke(ex.getMessage());
-        }
-    }
-
-    @ReactMethod
     public void logOut() {
         TwitterCore.getInstance().logOut();
     }
 
     private boolean hasValidKey(String key, ReadableMap options) {
         return options.hasKey(key) && !options.isNull(key);
-    }
-
-
-
-    class ReactNativeFabricApiClient extends TwitterApiClient {
-        public ReactNativeFabricApiClient(TwitterSession session) {
-            super(session);
-        }
-
-        /**
-         * Provide CustomService with defined endpoints
-         */
-        public CustomService getCustomService() {
-            return getService(CustomService.class);
-        }
-    }
-
-    // example users/show service endpoint
-    interface CustomService {
-        @GET("/1.1/users/show.json")
-        void show(@Query("user_id") long id, com.twitter.sdk.android.core.Callback<User> cb);
     }
 
 }
